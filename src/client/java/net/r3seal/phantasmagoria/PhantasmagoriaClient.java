cd1a52767f531fc0e3eb306f0c6a2f0c149b125d
SealyPrograming <lafar@DRAK01.(none)>
2025-02-07 21:35:38 +0100

    1.0.2 Added mod files

---
+++ src/client/java/net/r3seal/phantasmagoria/PhantasmagoriaClient.java
@@ -0,0 +1,10 @@
+package net.r3seal.phantasmagoria;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PhantasmagoriaClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/net/r3seal/phantasmagoria/PhantasmagoriaDataGenerator.java
@@ -0,0 +1,11 @@
+package net.r3seal.phantasmagoria;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PhantasmagoriaDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
